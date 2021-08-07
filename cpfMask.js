f6f1572d8417f4bb0c94e2a1ec861c3d48f3de01
30111971 <matheusmaica@gmail.com>
2021-08-07 19:29:48 -0300

    adicionando um programa de 'hello world' de diversas linguagens

--- cpfMask.js
+++ cpfMask.js
@@ -6,5 +6,3 @@ export const cpfMask = value => {
       .replace(/(\d{3})(\d{1,2})/, '$1-$2')
       .replace(/(-\d{2})\d+?$/, '$1')
 }
-
-// https://medium.com/trainingcenter/mascara-de-cpf-com-react-javascript-a07719345c93
